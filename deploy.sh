0f887a4a5ee2810f0880be88d1d91951b84a5718
wenzhiquan <zhiquanwen@xiaohongshu.com>
2018-05-30 11:47:57 +0800

    增加lib的下载，修改leancloud security配置

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+bookmark="./themes/next/source/lib/bookmark"
+reading_progress="./themes/next/source/lib/reading_progress"
+
+if [ ! -d "$bookmark" ];
+then
+    git clone https://github.com/theme-next/theme-next-bookmark.git ./themes/next/source/lib/bookmark
+fi
+
+if [ ! -d "$reading_progress" ];
+then
+    git clone https://github.com/theme-next/theme-next-reading-progress ./themes/next/source/lib/reading_progress
+fi
+
 hexo clean
 hexo generate
 gulp
